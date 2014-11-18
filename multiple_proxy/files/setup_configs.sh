94ec0f73c111c18841caed9abe5b7a24b6dce4b8
toto <toto@smartstudy.co.kr>
2014-11-18 15:40:44 +0900

    Add process which link logfile to stdout/sterr to setup_configs

--- multiple_proxy/files/setup_configs.sh
+++ multiple_proxy/files/setup_configs.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 perl ./generate_nginx_conf.pl
+touch /var/log/nginx/access.log /var/log/nginx/error.log
+ln -sf /dev/stdout /var/log/nginx/access.log
+ln -sf /dev/stderr /var/log/nginx/error.log
