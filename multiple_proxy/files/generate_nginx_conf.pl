83d9e863c9203a24fd3eabe17044f33b3e1f106c
toto <toto@smartstudy.co.kr>
2014-11-18 14:18:39 +0900

    remove proxy redirect off

--- multiple_proxy/files/generate_nginx_conf.pl
+++ multiple_proxy/files/generate_nginx_conf.pl
@@ -37,7 +37,6 @@ http {
     set_real_ip_from   12.0.0.0/8;
     real_ip_header     X-Forwarded-For;
 
-    proxy_redirect off;
     proxy_set_header Host $http_host;
     proxy_set_header X-Real-IP $remote_addr;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
